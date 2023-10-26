b87d264cd954055aa286cf4219d76f8afd2609b8
Eda Grace Paragoso <117576162+edaprgs@users.noreply.github.com>
2023-10-27 07:53:39 +0800

    Added changes on the HTML file

--- ssis_app/student/controller.py
+++ ssis_app/student/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, flash
 from ssis_app.student.forms import *
 import ssis_app.models as models
 from ssis_app.models.student import *
